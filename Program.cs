b90bba3970cf10780bdeadadde0c9aa5bb900081
AJPalanca <157432419+AJPalanca@users.noreply.github.com>
2024-02-05 17:44:31 -0500

    Homework1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double X = 2.5;
+        double Y = 3.3;
+
+        Console.WriteLine("X = 2.5, Y = 3.3");
+
+        double Z = 4 * Math.Pow(X, 2) + 3 * Y;
+
+        Console.WriteLine($"The value of Z is: {Z}");
+    }
+}
