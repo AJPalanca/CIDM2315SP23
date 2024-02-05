1212bde8a1c1f10f9d5292e42f17edfa4cd381dd
AJPalanca <157432419+AJPalanca@users.noreply.github.com>
2024-02-05 18:40:27 -0500

    Homework2Q1

---
+++ Homework2Q1.cs
@@ -0,0 +1,36 @@
+﻿namespace Homework2;
+class Program
+{
+    static void Main(string[] args)
+    {
+       Console.WriteLine("Please input a letter grade");
+       string letter_grade = Console.ReadLine();
+
+        if ("A" == letter_grade)
+        {
+        Console.WriteLine("GPA: 4");
+        }
+        else if("B" == letter_grade)
+        {
+        Console.WriteLine("GPA: 3");
+        }
+        else if("C" == letter_grade)
+        {
+        Console.WriteLine("GPA: 2");
+        }
+        else if("D" == letter_grade)
+        {
+        Console.WriteLine("GPA: 1");
+        }
+        else if("F" == letter_grade)
+        {
+        Console.WriteLine("GPA: 0");
+        }
+        else 
+        {
+        Console.WriteLine("Wrong Letter Grade!");
+        }
+    
+    }
+
+}
