babbfc6eb6ecaefcf99c3d69e805200fe1dfd7d3
AJPalanca <157432419+AJPalanca@users.noreply.github.com>
2024-02-12 22:40:10 -0500

    Homework4Q1

---
+++ Homework4Q1.cs
@@ -0,0 +1,28 @@
+﻿namespace Homework4Q1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 3;
+        int b = 5;
+
+        Console.WriteLine($"a = {a}; b={b}");
+
+        int largestNumber = FindLargestNumber(a, b);
+
+        Console.WriteLine($"The largest number is:{largestNumber}");
+    }
+
+    static int FindLargestNumber(int num1,int num2)
+    {
+        if (num1 > num2)
+        {
+        return num1;
+        }
+        else
+        {
+        return num2;
+        }
+    }
+}
