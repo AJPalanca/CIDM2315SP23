1af4fef7eb9fd4b2e62c84bbab3cef6393753153
AJPalanca <157432419+AJPalanca@users.noreply.github.com>
2024-02-19 20:37:41 -0500

    HomeWork5Q1
    Homework5Q1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Homework5Q1;
+
+class Program
+{
+    static void Main(string[] args)
+{
+        Console.WriteLine("Please input the first number:");
+        short number1 = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine("Please input the second number:");
+        short number2 = Convert.ToInt16(Console.ReadLine());
+
+        short largest = number1;
+
+        if (number2 > largest)
+        {
+            {
+                largest = number2;
+            }
+        }
+        else if (number2 > largest)
+        {
+            largest = number2;
+        }
+    
+        Console.WriteLine($"a = {number1}; b = {number2};");
+        Console.WriteLine("The largest number is: " + largest);
+    }
+}
